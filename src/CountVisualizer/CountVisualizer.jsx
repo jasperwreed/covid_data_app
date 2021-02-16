47a53a988ca0bc2ce8e25bc93fb1e68000fba040
jasperwreed <jmixon.reed@gmail.com>
2021-02-15 20:59:20 -0800

    displaying current covid cases us

--- src/CountVisualizer/CountVisualizer.jsx
+++ src/CountVisualizer/CountVisualizer.jsx
@@ -1,11 +1,30 @@
+import React, { useState, useEffect } from "react";
+import './CountVisualizer.css'
+
 const CountVisualizer = () => {
+  const [positiveCount, setPositiveCount] = useState(0);
+
+  useEffect(() => {
+    covidAPI();
+  });
+
+  const covidAPI = async () => {
+    const currentDataUrl = `https://api.covidtracking.com/v1/us/current.json`;
+    const data = await fetch(currentDataUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPositiveCount(data[0].positive);
+      });
+  };
+
   return (
     <>
-      <div>
-        <h1>Counter Goes Here</h1>
+      <div className='centerDiv'>
+        <h1 className='lgr-text'>{positiveCount}</h1>
       </div>
     </>
   );
 };
 
-export default CountVisualizer
+export default CountVisualizer;
