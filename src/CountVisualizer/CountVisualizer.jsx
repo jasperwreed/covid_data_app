2f7f6f15ac45ea6a8b731c833079242d7ded7d04
jasperwreed <jmixon.reed@gmail.com>
2021-02-16 20:48:38 -0800

    increase in pos cases from prev case date

--- src/CountVisualizer/CountVisualizer.jsx
+++ src/CountVisualizer/CountVisualizer.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import numberFormater from 'number-formatter'
-import dateformat from 'dateformat'
-import './CountVisualizer.css'
+import numberFormater from "number-formatter";
+import dateformat from "dateformat";
+import "./CountVisualizer.css";
 
 const CountVisualizer = () => {
   const [positiveCount, setPositiveCount] = useState(0);
-  const [caseDate, setCaseDate] = useState(0)
+  const [caseDate, setCaseDate] = useState(0);
+  const [positiveIncrease, setPositiveIncrease] = useState(0);
 
   useEffect(() => {
     covidAPI();
@@ -16,16 +17,25 @@ const CountVisualizer = () => {
     const data = await fetch(currentDataUrl)
       .then((response) => response.json())
       .then((data) => {
-        setCaseDate(data[0].dateChecked)
+        setCaseDate(data[0].dateChecked);
         setPositiveCount(data[0].positive);
+        setPositiveIncrease(data[0].positiveIncrease);
       });
   };
 
   return (
     <>
-      <div className='centerDiv'>
-        <h1 className='lgr-text'>{numberFormater("#,###." ,positiveCount)}</h1>
-        <p>Cumulative positive cases of covid as of {dateformat(caseDate, "dddd, mmmm dS, yyyy")} (US)</p>
+      <div className="centerDiv">
+        <div className='pad-b-100 bld-txt'>
+          <h1 className="lgr-text mar-b-0">
+            {numberFormater("#,###.", positiveCount)}
+          </h1>
+          <p className='md-txt'>+ {numberFormater("#,###." ,positiveIncrease)} from yesterday</p>
+        </div>
+        <p>
+          Cumulative positive cases of covid as of{" "}
+          {dateformat(caseDate, "dddd, mmmm dS, yyyy")} (US)
+        </p>
       </div>
     </>
   );
